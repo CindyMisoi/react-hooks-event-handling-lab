afaca38bbc85ba1c4e29dd29cf491a7563a40ad0
CindyMisoi <crmisoi2k@gmail.com>
2023-01-14 22:46:43 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe() {
+    function handleFocus(event){
+        console.log("Good!");
+    }
+
+    function handleBlur(event){
+        console.log("Hey! Eyes on me!");
+    }
+
+    return (
+        <div>
+          <button
+           id='button'
+           onFocus={handleFocus}
+           onBlur={handleBlur}
+          >
+            Eyes on me
+            </button>  
+        </div>
+    )
+}
+
+export default EyesOnMe
+
